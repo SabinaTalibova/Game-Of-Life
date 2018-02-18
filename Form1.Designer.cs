065dabc51fdc9573daedc44c150be1c8da234f99
SabinaTalibova <sabinatalibova1@gmail.com>
2018-02-19 02:25:36 +0400

    Clickable grid: done

---
+++ Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace GameofLife_Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.grp_grid = new System.Windows.Forms.GroupBox();
+            this.btn_start = new System.Windows.Forms.Button();
+            this.btn_stop = new System.Windows.Forms.Button();
+            this.btn_onestep = new System.Windows.Forms.Button();
+            this.btn_clear = new System.Windows.Forms.Button();
+            this.btn_export = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // grp_grid
+            // 
+            resources.ApplyResources(this.grp_grid, "grp_grid");
+            this.grp_grid.Name = "grp_grid";
+            this.grp_grid.TabStop = false;
+            // 
+            // btn_start
+            // 
+            this.btn_start.BackColor = System.Drawing.SystemColors.Control;
+            resources.ApplyResources(this.btn_start, "btn_start");
+            this.btn_start.Name = "btn_start";
+            this.btn_start.UseVisualStyleBackColor = false;
+            // 
+            // btn_stop
+            // 
+            resources.ApplyResources(this.btn_stop, "btn_stop");
+            this.btn_stop.Name = "btn_stop";
+            this.btn_stop.UseVisualStyleBackColor = true;
+            // 
+            // btn_onestep
+            // 
+            resources.ApplyResources(this.btn_onestep, "btn_onestep");
+            this.btn_onestep.Name = "btn_onestep";
+            this.btn_onestep.UseVisualStyleBackColor = true;
+            // 
+            // btn_clear
+            // 
+            resources.ApplyResources(this.btn_clear, "btn_clear");
+            this.btn_clear.Name = "btn_clear";
+            this.btn_clear.UseVisualStyleBackColor = true;
+            // 
+            // btn_export
+            // 
+            resources.ApplyResources(this.btn_export, "btn_export");
+            this.btn_export.Name = "btn_export";
+            this.btn_export.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btn_export);
+            this.Controls.Add(this.btn_clear);
+            this.Controls.Add(this.btn_onestep);
+            this.Controls.Add(this.btn_stop);
+            this.Controls.Add(this.btn_start);
+            this.Controls.Add(this.grp_grid);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grp_grid;
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.Button btn_stop;
+        private System.Windows.Forms.Button btn_onestep;
+        private System.Windows.Forms.Button btn_clear;
+        private System.Windows.Forms.Button btn_export;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameofLife_Test
+{
+    public partial class Form1 : Form
+    {
+        private const int button_dim = 14;
+        private const int grid_dim = 700;
+        private const int numof_cells = grid_dim / button_dim;
+        private static readonly Color ALIVE_CELL_COLOUR = Color.Green;
+        private static readonly Color DEAD_CELL_COLOUR = Color.Black;
+
+
+        int[,] engine;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            engine = new int[numof_cells, numof_cells];
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for(int i = 0; i + button_dim <= grid_dim; i += button_dim)
+            {
+                for (int j = 0; j + button_dim <= grid_dim; j+= button_dim)
+                {
+                    Button cell_Button = new Button();
+                    cell_Button.Size = new Size(button_dim, button_dim);
+                    cell_Button.Location = new Point(i, j);
+                    cell_Button.Click += new EventHandler(ClickCell);
+                    cell_Button.FlatStyle = FlatStyle.Flat;
+                    cell_Button.BackColor = Color.Black;
+                    cell_Button.FlatAppearance.BorderColor = Color.Black;
+                    cell_Button.FlatAppearance.BorderSize = 1;
+                    grp_grid.Controls.Add(cell_Button);
+                    
+
+
+                }
+            }
+        }
+
+        private void ClickCell(object sender, EventArgs e)
+        {
+            Control ctrl = ((Control)sender);
+            switch (ctrl.BackColor.Name)
+            {
+                case "Black":
+                    ctrl.BackColor = Color.Green;
+                    break;
+                case "Green":
+                    ctrl.BackColor = Color.Black;
+                    break;
+
+                default:
+                    ctrl.BackColor = Color.Red;
+                    break;
+
+
+            }
+
+
+
+
+        }
+    }
+
+}
+
+         
+
+   
+
